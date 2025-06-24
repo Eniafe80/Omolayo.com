5f1713b524e629e37305703c0d6e12cdffc7f832
Eniafe80 <oluwadamilola4357@gmail.com>
2025-06-24 03:26:11 +0100

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// Toggle sidebar open/close
 function toggleMenu() {
   document.body.classList.toggle('menu-open');
 }
 
+// Navigate back to homepage
 function goHome() {
   window.location.href = 'index.html';
 }
